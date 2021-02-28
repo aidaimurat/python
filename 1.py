aad0d1ff8db56ced1fa9165b2d6fbb5d3226b6ca
aidaimurat <64561162+aidaimurat@users.noreply.github.com>
2021-02-28 18:59:40 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,9 +1,9 @@
 #1
-lst = [92, 91, 49, 87, 74, 20, 94, 12, 64, 36, 97, 2, 96, 40, 97, 36, 32, 22, 80, 83, 49, 52, 62, 31,
-55, 86, 84, 1, 22, 15, 52, 18, 78, 92, 21, 9, 85, 89, 54, 99, 80, 7, 4, 31, 30, 28, 59, 35, 72, 33]
-
-dict(enumerate(lst))
-print({index+1:value for index,value in enumerate(lst)})
+lst = [92, 91, 49, 87, 74, 20, 94, 12, 64, 36, 97, 2, 96, 40, 97, 36, 32, 22, 80, 83, 49, 52, 62, 31,55, 86, 84, 1, 22, 15, 52, 18, 78, 92, 21, 9, 85, 89, 54, 99, 80, 7, 4, 31, 30, 28, 59, 35, 72, 33]
+dic = {}
+for index,value in enumerate(lst):
+    dic[index]=value
+print(dic)
 
 #2
 import random
@@ -31,8 +31,9 @@ else:
     print("Все, вы не выиграли. Игра завершилась")
 
 #3
-some_string = "enviroment"
-print(some_string[4:7])
+some_word = input("Введите имя нечетной длины больше 7 символов: ")
+number_word = int((len(some_word) // 2) + (1))
+print(some_word[number_word-2]+some_word[number_word-1]+some_word[number_word])
 
 #4
 a = "Aidana"
